b8e669816beb684bea9634b83d5f5ffa7d51c9a2
Matthew Umana <mumana98@gmail.com>
2019-11-02 14:03:40 -0500

    added components

---
+++ my-app/src/compontents/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class dashboard extends React{
+    constructor(props){
+        super(props)
+        
+    }
+    
+    
+    render()
+}
+
+export default dashboard
